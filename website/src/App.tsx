b535df8e3b2fe5f42bc25cb2dc8056a8d3b1761e
James Thorniley <james.thorniley@gmail.com>
2024-12-19 18:59:28 +0000

    do some dynamic importing

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -5,13 +5,11 @@ import geojson from "geojson";
 import L from "leaflet";
 import React from "react";
 
-import {wards, wardTooltips} from "./wards";
-
-import oxfordDivisions from "./data/oxford-divisions.json";
-
 function App() {
   const mapRef = React.useRef<HTMLElement>(null);
-  
+  const [map, setMap] = React.useState<L.Map | null>(null)
+  const [tt, setTT] = React.useState<React.ReactNode | null>(null);
+
   React.useEffect(() => {
 
       while (mapRef.current!.firstChild !== null) {
@@ -20,43 +18,56 @@ function App() {
       const mapEl = document.createElement("div")
       mapRef.current!.appendChild(mapEl);
 
-      const map = L.map(mapEl);
+      const newMap = L.map(mapEl);
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      }).addTo(map);
-      
-      wards.addTo(map);
+      }).addTo(newMap);
 
-      L.geoJSON(oxfordDivisions as geojson.GeoJsonObject,
-        {
-          style: () => {
 
-            return {
-              stroke: true,
-              color: "black",
-              weight: 4
-            }
-          },
-          interactive: false,
-          onEachFeature: (feature, layer) => {
-            let name = feature.properties.Name;
-            name = name.substring(0, name.length -3)
-            layer.bindTooltip(name, {permanent: true, direction: "center"})
-            
-          },
-        }
-      ).addTo(map);
+      newMap.setView([51.746409, -1.235782], 14)
 
-      map.setView([51.746409, -1.235782], 14)
+      setMap(newMap)
     }
-  , [])
+  , [setMap])
   
+  React.useEffect(() => {
+    (async () => {
+      if (map){
+        const {wards, wardTooltips} = await import("./wards");
+
+        wards.addTo(map);
+
+        const oxfordDivisions = await import( "./data/oxford-divisions.json");
+        L.geoJSON(oxfordDivisions as geojson.GeoJsonObject,
+          {
+            style: () => {
+
+              return {
+                stroke: true,
+                color: "black",
+                weight: 4
+              }
+            },
+            interactive: false,
+            onEachFeature: (feature, layer) => {
+              let name = feature.properties.Name;
+              name = name.substring(0, name.length -3)
+              layer.bindTooltip(name, {permanent: true, direction: "center"})
+              
+            },
+          }
+        ).addTo(map);
+
+        setTT(wardTooltips);
+      }
+    })()
+  }, [map])
   return (
     <>
       <main className="app">
-        <section style={{display: "none"}}>{ wardTooltips }</section>
+        (tt && <section style={{display: "none"}}>{ tt }</section>)
         <section id="map" ref={mapRef}></section>
       </main >
     </>
